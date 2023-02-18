cb23ff03d112dd43e05938822d8852b1e03d18cc
jeremyAparicio <jeremy.aparicio.g@gmail.com>
2023-02-18 21:03:08 +0000

    Se creo endpoints

--- src/app.py
+++ src/app.py
@@ -9,7 +9,9 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+from models import Personajes
+from models import Favoritos
+from models import Continentes
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -39,11 +41,79 @@ def sitemap():
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    all_users = User.query.all()
+    """
+    new_users = []
+    for i in range(len(all_users)):
+        print(all_users[i])
+        new_users.append(all_users[i].serialize())
+    """
+    all_users = list(map(lambda user: user.serialize() ,all_users))
 
-    return jsonify(response_body), 200
+    return jsonify(all_users), 200
+
+@app.route("/personajes", methods=["GET"])
+def get_all_perso():
+
+    return jsonify({
+        "mensaje": "Listado de los personajes"
+    })
+
+@app.route("/personajes/<int:id>", methods=["GET"])
+def get_one_perso(id):
+
+    return jsonify({
+        "mensaje": "Información del personaje basandose en su id "+str(id)
+    })
+
+@app.route("/continentes", methods=["GET"])
+def get_all_cont():
+
+    return jsonify({
+        "mensaje": "Listado de continentes"
+    })
+
+@app.route("/favoritos", methods=["GET"])
+def get_all_fav():
+
+    return jsonify({
+        "mensaje": "Listado de items en favoritos"
+    })
+
+@app.route("/continentes/<int:id>", methods=["GET"])
+def get_one_cont(id):
+
+    return jsonify({
+        "mensaje": "aca estara la info del continente con id "+str(id)
+    })
+
+@app.route("/favoritos/personajes/<int:personajes_id>", methods=['POST'])
+def post_fav_perso(personajes_id):
+    
+    return jsonify({
+        "mensaje": "el personaje con id "+ str(personajes_id) + " ha sido agregado"
+    })
+
+@app.route("/favoritos/personajes/<int:personajes_id>", methods=['DELETE'])
+def delete_fav_perso(personajes_id):
+    
+    return jsonify({
+        "mensaje": "el personaje con id "+ str(personajes_id) + " ha sido eliminado"
+    })
+
+@app.route("/favoritos/continentes/<int:continentes_id>", methods=['POST'])
+def post_fav_cont(continentes_id):
+    
+    return jsonify({
+        "mensaje": "el continente con id "+ str(continentes_id) + " ha sido agregado"
+    })
+
+@app.route("/favoritos/continentes/<int:continentes_id>", methods=['DELETE'])
+def delete_fav_cont(continentes_id):
+    
+    return jsonify({
+        "mensaje": "el continente con id "+ str(continentes_id) + " ha sido eliminado"
+    })
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,6 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String
 
 db = SQLAlchemy()
+
+Base = declarative_base()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,3 +21,29 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+        
+class Personajes(Base):
+    __tablename__ = 'personaje'
+    id_personaje = Column(Integer, primary_key=True)
+    nombre = Column(String(250))
+    titulo = Column(String(250))
+    casa= Column(String(250), nullable=False)
+    perso_a_casa = Column(Integer, ForeignKey('casa.id_casa'))
+    perso_a_conti = Column(Integer, ForeignKey('continente.id_continente'))
+    perso_a_fav = Column(Integer, ForeignKey('favorito.id_favoritos'))
+
+class Casas(Base):
+    __tablename__ = 'casa'
+    id_casa = Column(Integer, primary_key=True)
+    nombre = Column(String(250))
+
+class Continentes(Base):
+    __tablename__ = 'continente'
+    id_continente = Column(Integer, primary_key=True)
+    nombre = Column(String(250))
+    conti_a_fav = Column(Integer, ForeignKey('favorito.id_favoritos'))
+
+class Favoritos(Base):
+    __tablename__ = 'favorito'
+    id_favoritos = Column(Integer, primary_key=True)
+    user_a_fav = Column(Integer, ForeignKey('usuario.id_usuario'))
